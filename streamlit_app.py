99599aeeaa1e66ce775df5b0fa3ec1bbe25c30e6
ADITYA POKHARKAR <83746376+Aadi1007@users.noreply.github.com>
2023-06-03 15:45:29 +0530

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -31,6 +31,21 @@ fruits_to_show = my_fruit_list.loc[fruits_selected]
 
 
 
+
+  
+  
+my_cnx = snowflake.connector.connect(**streamlit.secrets["snowflake"])
+my_cur = my_cnx.cursor()
+my_cur.execute("select * from pc_rivery_db.public.fruit_load_list")
+my_data_rows = my_cur.fetchall()
+streamlit.header("The fruit load list contains:")
+streamlit.dataframe(my_data_rows)
+
+add_my_fruit = streamlit.text_input('What fruit would you like to have?')
+streamlit.write('The user entered', add_my_fruit)
+
+my_cur.execute("insert into fruit_load_list values ('from streamlit')")
+
 def get_fruityvice_data(this_fruit_choice):
   fruityvice_response = requests.get("https://fruityvice.com/api/fruit/" + this_fruit_choice)
   fruityvice_normalized = pandas.json_normalize(fruityvice_response.json())
@@ -45,19 +60,7 @@ try:
   else:
     back_from_function = get_fruityvice_data(fruit_choice)
     streamlit.dataframe(back_from_function)
-  
-  
-my_cnx = snowflake.connector.connect(**streamlit.secrets["snowflake"])
-my_cur = my_cnx.cursor()
-my_cur.execute("select * from pc_rivery_db.public.fruit_load_list")
-my_data_rows = my_cur.fetchall()
-streamlit.header("The fruit load list contains:")
-streamlit.dataframe(my_data_rows)
-
-add_my_fruit = streamlit.text_input('What fruit would you like to have?')
-streamlit.write('The user entered', add_my_fruit)
-
-my_cur.execute("insert into fruit_load_list values ('from streamlit')")
+    
 streamlit.stop()
 
 
