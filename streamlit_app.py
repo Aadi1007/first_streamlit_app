7fd4db448505b2e75d57d23ef920b2ef5fe3a099
ADITYA POKHARKAR <83746376+Aadi1007@users.noreply.github.com>
2023-06-03 15:41:18 +0530

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -47,10 +47,6 @@ try:
     streamlit.dataframe(back_from_function)
   
   
-  
-
-
-
 my_cnx = snowflake.connector.connect(**streamlit.secrets["snowflake"])
 my_cur = my_cnx.cursor()
 my_cur.execute("select * from pc_rivery_db.public.fruit_load_list")
